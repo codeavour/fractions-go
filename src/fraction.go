b958b0d67b2d78601c22afd8408d4791c5c7e121
Ricardo Padrela <rpadrela@gmail.com>
2020-11-17 17:54:56 +0000

    Add zero fractions implemented

--- src/fraction.go
+++ src/fraction.go
@@ -1,2 +1,21 @@
 // Package fraction contains fraction stuff.
 package fraction
+
+type Fraction struct {
+	numerator   int
+	denominator int
+}
+
+func NewFraction(numerator, denominator int) Fraction {
+	return Fraction{
+		numerator:   numerator,
+		denominator: denominator,
+	}
+}
+
+func (f Fraction) Add(addend Fraction) Fraction {
+	return Fraction{
+		numerator:   0,
+		denominator: 1,
+	}
+}
--- src/fraction_test.go
+++ src/fraction_test.go
@@ -1,1 +1,31 @@
 package fraction_test
+
+import (
+	"testing"
+
+	fraction "github.com/codeavour/fractions-go/src"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddingFractions(t *testing.T) {
+	testCases := []struct {
+		name        string
+		leftAddend  fraction.Fraction
+		rightAddend fraction.Fraction
+		sum         fraction.Fraction
+	}{
+		{
+			name:        "zero fractions",
+			leftAddend:  fraction.NewFraction(0, 1),
+			rightAddend: fraction.NewFraction(0, 1),
+			sum:         fraction.NewFraction(0, 1),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			assert.Equal(t, testCase.sum, testCase.leftAddend.Add(testCase.rightAddend), testCase.name)
+		})
+	}
+}
