a1a714718becdf64a51680f28958e6206c686292
Ricardo Padrela <rpadrela@gmail.com>
2020-11-22 17:37:19 +0000

    linting

--- src/fraction_test.go
+++ src/fraction_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//nolint: scopelint
+//nolint: scopelint, funlen
 func TestAddingFractions(t *testing.T) {
 	testCases := []struct {
 		name        string
